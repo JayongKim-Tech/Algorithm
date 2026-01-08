6da74d01dabd1e054a32c0a0c5ebd2b4500e2223
mwson <rlawkdyd13@gmail.com>
2026-01-08 07:23:18 -0600

    2738

--- BOJ/Class_01/2438.cpp
+++ BOJ/Class_01/2438.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+    // 1. C 입출력과의 동기화를 끊어서 속도를 획기적으로 높임
+    ios::sync_with_stdio(false);
+    
+    // 2. 입력과 출력의 연결을 끊어서 입력 대기 시간을 줄임
+    cin.tie(NULL);
+
     int a;
     cin >> a;
 
